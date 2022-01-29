f28320bb21bb026002727e4a749f775f06858919
keithliu03 <38765153+keithliu03@users.noreply.github.com>
2022-01-28 19:09:45 -0800

    working on modeswap

--- Assets/Scripts/ModeSwap.cs
+++ Assets/Scripts/ModeSwap.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class ModeSwap : MonoBehaviour
 {
+
+    public GameObject[] lightMode;
+    public GameObject[] darkMode;
+
+    public Renderer rend;
+
+    private bool lightModeOn = true;
+    private bool darkModeOn = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lightMode = GameObject.FindGameObjectsWithTag("LightTiles");
+        darkMode = GameObject.FindGameObjectsWithTag("DarkTiles");
     }
 
     // Update is called once per frame
@@ -16,6 +26,18 @@ public class ModeSwap : MonoBehaviour
         if (Input.GetKeyDown("q"))
         {
             print("space key was pressed");
+        }
+    }
+
+    public void toggleModes()
+    {
+        if (lightModeOn == true && darkModeOn == false)
+        {
+            foreach (var item in lightMode)
+            {
+                //item.GetComponent<Renderer>().disabled;
+            }
+            
         }
     }
 }
