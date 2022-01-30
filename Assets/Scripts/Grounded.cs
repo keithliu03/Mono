d743dec0d394dab82503d39065a0fbd9a20668c9
Spice <mvanlaethem0@gmail.com>
2022-01-29 18:00:35 -0800

    Added animations

--- Assets/Scripts/Grounded.cs
+++ Assets/Scripts/Grounded.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Grounded : MonoBehaviour
 {
     public GameObject player;
+    public Animator animator;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "LightTiles" || collision.collider.tag == "DarkTiles")
         {
             player.GetComponent<Player>().isGrounded = true;
+            animator.SetBool("Grounded", true);
         }
     }
 
@@ -19,6 +21,7 @@ public class Grounded : MonoBehaviour
         if (collision.collider.tag == "LightTiles" || collision.collider.tag == "DarkTiles")
         {
             player.GetComponent<Player>().isGrounded = false;
+            animator.SetBool("Grounded", false);
         }
     }
 }
